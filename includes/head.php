0ea80c4379e8922d02e3565c747799b158b134c6
NinjaGrisen <wallof.kristofer@gmail.com>
2015-02-04 17:29:35 +0100

    Start

--- includes/head.php
+++ includes/head.php
@@ -2,10 +2,13 @@
 <html>
 <head>
 	<title></title>
+	<link rel="stylesheet" type="text/css" href="style/foundation.min.css" />
+	<link rel="stylesheet" type="text/css" href="style/normalize.css" />
+	<link rel="stylesheet" type="text/css" href="style/fondation-change.less" />
 	<link rel="stylesheet/less" type="text/css" href="style/main.less" />
-	<script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
+	<script type="text/javascript" src="js/vendor/jquery.js"></script>
+	<script type="text/javascript" src="js/vendor/modernizr.js"></script>
 	<script type="text/javascript" src="js/less.js"></script>
 </head>
 <body>
 
-<h1>Header!</h1>
--- index.php
+++ index.php
@@ -1,10 +1,35 @@
 <?php
 include("includes/head.php");
 ?>
+<div id="sidebar">
+	<form>
+		<input type="text" name="search" id="search">
+	</form>
+	<ul>
+		<li>Bucket</li>
+		<li>Profile</li>
+		<li>Friends</li>
+		<li>Milestones</li>
+		<li>Completed</li>
+		<li>Recomended</li>
+	</ul>
+	<div class="poo"></div>
 
-<p>hej</p>
-
-
+</div>
+	<div id="content">
+	<div class="row">
+		<div class="larger-12 columns bucket">
+			<p>hej</p>
+			<h1>hej</h1>
+			<h2>hej</h2>
+			<h3>hej</h3>
+			<h4>hej</h4>
+			<h5>hej</h5>
+			<h6>hej</h6>
+			<a href="#">hej</a>
+		</div>
+	</div>
+</div>
 <?php
 include("includes/footer.php");
 ?>
