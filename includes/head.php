e6dd9836bab326ac0a5dae487d1ccbb161ab6d9c
NinjaGrisen <wallof.kristofer@gmail.com>
2015-02-04 14:52:38 +0100

    Grunderna
    Less, js och includes

---
+++ includes/head.php
@@ -0,0 +1,9 @@
+<<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet/less" type="text/css" href="style/main.less" />
+	<script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
+	<script type="text/javascript" src="js/less.js"></script>
+</head>
+<body>
