cea5e9e35793eba516aabc29b6a7e46ea7d3344f
NinjaGrisen <wallof.kristofer@gmail.com>
2015-02-05 14:36:17 +0100

    Start av Less array
    Array för kategorierna

--- includes/head.php
+++ includes/head.php
@@ -4,7 +4,8 @@
 	<title></title>
 	<link rel="stylesheet" type="text/css" href="style/foundation.min.css" />
 	<link rel="stylesheet" type="text/css" href="style/normalize.css" />
-	<link rel="stylesheet" type="text/css" href="style/fondation-change.less" />
+	<link rel="stylesheet/less" type="text/css" href="style/fondation-change.less" />		
+	<link rel="stylesheet/less" type="text/css" href="style/includes/array.less" />
 	<link rel="stylesheet/less" type="text/css" href="style/main.less" />
 	<script type="text/javascript" src="js/vendor/jquery.js"></script>
 	<script type="text/javascript" src="js/vendor/modernizr.js"></script>
