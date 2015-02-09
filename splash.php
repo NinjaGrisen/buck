b1b0a0a8bf13a3dae6b41cda6a7cf55fcd9b4a1b
Ademaz <johanlevin22@gmail.com>
2015-02-09 15:36:35 +0100

    börjat på splash

---
+++ splash.php
@@ -0,0 +1,24 @@
+<?php
+include("includes/head.php");
+?>
+
+
+<div class="row">
+	<div class="large-4 columns login">
+		<form>
+			<label>Username</label>
+			<input type="text">
+			<label>Password</label>
+			<input type="text">
+		</form>
+	</div>
+
+	<div class="large-4 columns socialMedia">
+		some bullshit right here
+	</div>
+</div>
+
+
+<?php
+include("includes/footer.php");
+?>
