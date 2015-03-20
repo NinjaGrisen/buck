da8da787762eec7d2b1d87b6c73f543a1ab5819c
StaffanJ <staffan.jansson@outlook.com>
2015-03-20 10:22:33 +0100

    Olika items för testing.
    Har gjort en array av fem olika items för att testa drag och sortering.

---
+++ php/listitem.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 0.2b
+ */
+
+//
+// Database `bucketlist`
+//
+
+// `bucketlist`.`listitem`
+$listitem = array(
+  array('ItemID' => '1','CategoryID' => '1','Title' => 'First item.','Desc' => 'First item.','Checked' => '0','Rank' => '7','Private' => '1'),
+  array('ItemID' => '2','CategoryID' => '2','Title' => 'Going jumping!','Desc' => 'Go jump from a cliff!','Checked' => '1','Rank' => '1','Private' => '1'),
+  array('ItemID' => '3','CategoryID' => '3','Title' => 'Help in a war!','Desc' => 'Help fight the evil!','Checked' => '0','Rank' => '2','Private' => '1'),
+  array('ItemID' => '4','CategoryID' => '3','Title' => 'Have a drink!','Desc' => 'Drink something good.','Checked' => '1','Rank' => '3','Private' => '1'),
+  array('ItemID' => '5','CategoryID' => '4','Title' => 'Go travel!','Desc' => 'Travel to a foreign country!','Checked' => '0','Rank' => '4','Private' => '1')
+);
