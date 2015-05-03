410e205e6137510e7d58adb0e9964489a1fbf3da
StaffanJ <staffan.jansson@outlook.com>
2015-05-03 14:45:03 +0200

    Lägger till ajax.

--- js/ajax.js
+++ js/ajax.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 	var buckets = $('.buckets'),
+		listmanagement = $('#listManagement')
 		bucketListAppend = $('#bucket-item-template').html();
 
 
@@ -29,5 +30,54 @@ $(document).ready(function(){
 	buckets.delegate('.edit-list-item', 'click', function(){
 			$(this).parents('.bucket-list-item').toggleClass('expand-edit');
 		});
+
+	//För list.php, list hantering som uppdatering, hämta listor och lägga till.
+
+	$('#getlist').click(function(){
+
+		$('#result').html('');
+
+		$.ajax({
+			url : 'php/getList.php',
+			dataType : 'json', //text
+			
+			success: function(data){
+				console.log(data);
+				$.each(data, function(i){
+					$('#result').append('<p class="pointer" id="' + data[i].ItemID + '">' + data[i].Title + '</p>');
+					console.log('Printing out the data.');
+				});
+			},
+			error: function(){
+
+			},
+		});
+	});
+
+	listmanagement.delegate('#result p', 'click', function(){
+		var result = $(this).text(),
+			list_id = $(this).attr('id');
+
+		$(this).replaceWith('<input id="updated" type="text" value="' + result + '"/> <input id="update" value="Update" type="submit" />');
+		console.log('Clicking to change p element.');
+
+		$('#update').click(function(){
+			var title = $('#updated').val();
+
+			$.ajax({
+				type : 'POST',
+				url : 'php/updateList.php',
+				data : { title: title, list_id: list_id},
+				success: function(data){
+					$('#updated').replaceWith('<p class="pointer" id="' + data.ItemID + '">' + data.Title + '</p>');
+					$('#update').remove();
+					console.log('Printing out the updated data.');
+				},
+				error: function(){
+
+				},
+			});
+		});
+	});
 
 });
--- list.php
+++ list.php
@@ -1,39 +1,13 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Ajax list</title>
-
-	<script src="js/jquery.js"></script>
-	<script src="js/ajax.js"></script>
-	<link rel="stylesheet" type="text/css" href="css/myCSS.css">
-
-</head>
-<body>
+<?php
+include("includes/head.php");
+?>
 <div id='listManagement'>
-<form id='userForm'>
-	Username:	<input type='text' name='username' /><br>
-	Password:	<input type='password' name='password' /><br>
-	First name:	<input type='text' name='fname' /><br>
-	Last name:	<input type='text' name='lname' /><br>
-	Age:		<input type='date' name='age' /><br>
-	Country:	<input type='text' name='country' /><br>
-	Gender:		<select name='gender'>
-  					<option value="Female">Female</option>
-  					<option value="Male">Male</option>
-				</select><br>
-	<input type="button" value='Get Users' id='getuser'><br>
-	<input type="button" value='Add Users' id='adduser'>
-</form>
+
 <input type="button" value='Get List' id='getlist'><br>
 <div id='result'>
 <p class='pointer'>Here the result will come out!</p>
 </div>
 </div>
-</body>
-</html>
-
 <?php
-
-
-
+include("includes/footer.php");
 ?>
