599ce95eb7c3b53a2bbc2fdbe5a56b711e44ffbd
StaffanJ <staffan.jansson@outlook.com>
2015-05-03 14:29:34 +0200

    Extern databas.
    Flyttar över allt till en extern databas.

--- php/db.php
+++ php/db.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $dbh = new PDO('mysql:host=localhost;dbname=bucketlist', 'root', '');
+    $dbh = new PDO('mysql:host=mysql08.citynetwork.se;dbname=119897-bucketlist', '119897-ky15282', 'Ajabaja123');
 } catch (PDOException $e) {
     print "Error!: " . $e->getMessage() . "<br/>";
     die();
