665c81caf01c7d50e7b4a832506824ed1c37af0c
Ademaz <johanlevin22@gmail.com>
2015-05-20 14:26:10 +0200

    wat

--- profile.php
+++ profile.php
@@ -6,36 +6,48 @@ include("includes/sidebar.php");
 <div id="container" >
 	<div class="row">
 		<div class="large-12 medium-12 small-12 page-header">
-			<h1>Master of Music's Bucket List</h1>
+			<h1>MasterOfMusic's Bucket List</h1>
 		</div>
 	</div>
 
 	<div class="row">
 		<div class="large-12 medium-12 small-12 columns profile-box">
-			<ul class="profile-list-bold">
-				<li>First Name</li>
-				<li>Last Name</li>
-				<li>Email</li>
-				<li>Nationality</li>
-				<li>Date of Birth</li>
-				<li>Gender</li>
-				<li>Bucket List Items</li>
-			</ul>
-
-			<ul class="profile-list">
-				<li>Ludwig</li>
-				<li>von Beethoven</li>
-				<li>LudwigVonSwag@gmail.com</li>
-				<li>Austrian</li>
-				<li>1770-Dec-17</li>
-				<li>Male</li>
-				<li>35</li>
-			</ul>
-
+			<img class="edit-profile" src="img/cogwheel.png">
 			<img src="img/profile-placeholder.png" class="profile-picture">
+			<table>
+				<tr>
+					<td class="profile-left">First Name</td>
+					<td>Ludwig</td>
+				</tr>
+				<tr>
+					<td class="profile-left">Last Name</td>
+					<td>von Beethoven</td>
+				</tr>
+				<tr>
+					<td class="profile-left">Email</td>
+					<td>LudwigVonSwag@gmail.com</td>
+				</tr>
+				<tr>
+					<td class="profile-left">Nationality</td>
+					<td>Austrian</td>
+				</tr>
+				<tr>
+					<td class="profile-left">Date of Birth</td>
+					<td>1770-Dec-17</td>
+				</tr>
+				<tr>
+					<td class="profile-left">Gender</td>
+					<td>Male</td>
+				</tr>
+				<tr>
+					<td class="profile-left">Bucket List Items</td>
+					<td>35</td>
+				</tr>
+			</table>
+
 
 			<div class="large-9 columns about-container">
-				<p class="about">About</p>
+				<p class="about">About MasterOfMusic</p>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis consectetur lorem. Nunc ullamcorper nibh id erat rutrum, quis iaculis ligula commodo. Maecenas pharetra erat consequat vulputate commodo. Sed vestibulum massa in sem ultricies posuere. Morbi sed neque eget mauris consequat molestie ut vitae odio. Mauris sapien sem, dictum et enim vel, ultrices finibus massa. Vestibulum vitae elementum magna. Donec a quam non tortor efficitur tempor eu eget leo. Cras ac turpis at nibh congue posuere.</p>
 			</div>
 		</div>
